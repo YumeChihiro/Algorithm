5edeb513b8ee4ee007458a4064da087dd727c2c2
YumeChihiro <142805719+YumeChihiro@users.noreply.github.com>
2024-01-11 19:54:22 +0700

    Update BubbleSort.java

--- BubbleSort.java
+++ BubbleSort.java
@@ -1,7 +1,10 @@
 import java.util.Scanner;
 
-//Giải thuật sắp xếp đơn giản nhất
-//So sánh các cắp phần tử liền kề
+//Giải thuật sắp xếp đơn giản nhất, không phù hợp với tập dữ liệu lớn (0(n^2))
+//So sánh các cắp phần tử liền kề và tráo đổi chúng nếu không theo thứ tự
+
+//The simplest sorting algorithm, not suitable for large data sets (0(n^2))
+//Compare adjacent elements and swap them if out of order
 public class BubbleSort{
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
@@ -11,7 +14,7 @@ public class BubbleSort{
             arr[i] = scan.nextInt();
 
         for(int i=n-1;i>=1;i--) {
-            boolean check = false;
+            boolean check = true; //giảm sự tráo đổi khi mảng này đã được sắp xếp
             for(int j=0;j<i;j++) {
                 if(arr[j]>arr[j+1]) {
                     int temp = arr[j];
